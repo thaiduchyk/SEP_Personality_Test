f42c0619c62e23bee3daaa61239d541a1174d429
thaiduchyk <thaiduchyk@gmail.com>
2016-03-20 20:50:46 +0200

    add test to registrations controller

--- spec/controllers/api/v1/auth/registrations_controller_spec.rb
+++ spec/controllers/api/v1/auth/registrations_controller_spec.rb
@@ -6,16 +6,19 @@ RSpec.describe Api::V1::Auth::RegistrationsController, type: :controller do
     @request.env['devise.mapping'] = Devise.mappings[:user]
   end
 
-  context 'with valid parameters' do
+  let(:user_attributes){ FactoryGirl.attributes_for(:user) }
 
-    let(:user_attributes){ FactoryGirl.attributes_for(:user) }
+  let(:invalid_attributes){ FactoryGirl.attributes_for(:user_not_valid) }
+
+  context 'with valid parameters' do
+    user = FactoryGirl.build(:user)
 
     it 'signs in user' do
       post :create, user_attributes
-      expect(subject.current_user).to_not eq(nil)
+      expect(subject.current_user.email).to eq(user.email)
     end
 
-    it 'responds status with status 200' do
+    it 'responds with status 200' do
       post :create, user_attributes
       expect(response.status).to eq(200)
     end
@@ -24,23 +27,39 @@ RSpec.describe Api::V1::Auth::RegistrationsController, type: :controller do
       expect{ post :create, user_attributes }.to change(User, :count).by(1)
     end
 
-    it 'renders correct json'do
+    it 'renders correct json' do
     end
   end
 
   context 'with invalid parameters' do
 
     it 'doesnt save user' do
-      expect{post :create, {}}.to_not change(User, :count)
+      expect{post :create, invalid_attributes}.to_not change(User, :count)
     end
 
-    it 'doesnt sign in user' do
-      post :create, {}
-      expect(subject.current_user).to eq(nil)
+    it 'responds with status 403' do
+      post :create, invalid_attributes
+      expect(response.status).to eq(403)
     end
 
-    it 'error return if email is already in use' do
-    end
+    it 'responds with error message'
 
   end
+
+  context 'with existing email' do
+    before (:each) do
+      @user = FactoryGirl.create(:user)
+    end
+
+    it 'doesnt save user' do
+      expect{post :create, user_attributes}.to_not change(User, :count)
+    end
+
+    it 'responds with status 403' do
+      post :create, user_attributes
+      expect(response.status).to eq(403)
+    end
+  end
+
+
 end
