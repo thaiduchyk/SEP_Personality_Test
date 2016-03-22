e31c10cd0c1a41d6183c437e8289246e0c70b141
Dmitri <orldm@mail.ru>
2016-03-22 18:05:33 +0200

    add api paths

--- app/assets/javascripts/config/ng-token-auth.config.js
+++ app/assets/javascripts/config/ng-token-auth.config.js
@@ -2,6 +2,8 @@ angular.module('ng-token-auth.config', ['ng-token-auth'])
 .config(function($authProvider) {
   $authProvider.configure({
     apiUrl: 'http://localhost:3000',
+    emailSignInPath: '/api/v1/auth/sign_in',
+    signOutUrl: '/api/v1/auth/sign_out',
     authProviderPaths: {
       facebook: '/api/v1/auth/facebook',
       linkedin: '/api/v1/auth/linkedin'
