5f849d2b506fef452fb40c9edaedef52ecc8ed61
thaiduchyk <thaiduchyk@gmail.com>
2016-03-22 18:09:13 +0200

    Add test for registrations controller

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
     params.permit(:email, :password, :name, :surname, :password_confirmation)
   end
 
-  swagger_controller :registrations, "User Management"
+  swagger_controller :registrations, "User form registration"
 
   swagger_api :create do
     summary "Creates new user"
@@ -13,9 +13,8 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
     param :form, :email, :string, :required, "Email"
     param :form, :password, :string, :required, "Password"
     param :form, :password_confirmation, :string, :required, "Password confirmation"
-    response :unauthorized
-    response :not_acceptable, "The request you made is not acceptable"
-
+    response :forbidden
+    
   end
 
 end
