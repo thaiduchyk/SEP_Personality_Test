1e0d19d880c7e53c79aa16179f774748cc47cf65
thaiduchyk <thaiduchyk@gmail.com>
2016-03-26 16:20:28 +0200

    Refactor session controller

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -18,51 +18,10 @@ class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
     response :not_found
   end
 
+
   def create
-
-    # Check
-    field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
-
-    @resource = nil
-    if field
-      q_value = resource_params[field]
-
-      if resource_class.case_insensitive_keys.include?(field)
-        q_value.downcase!
-      end
-
-      q = "#{field.to_s} = ? AND provider='email'"
-
-      if ActiveRecord::Base.connection.adapter_name.downcase.starts_with? 'mysql'
-        q = "BINARY " + q
-      end
-
-      @resource = resource_class.where(q, q_value).first
-    end
-
-    if @resource and valid_params?(field, q_value) and @resource.valid_password?(resource_params[:password]) and (!@resource.respond_to?(:active_for_authentication?) or @resource.active_for_authentication?)
-      # create client id
-      @client_id = SecureRandom.urlsafe_base64(nil, false)
-      @token     = SecureRandom.urlsafe_base64(nil, false)
-
-      @resource.tokens[@client_id] = {
-          token: BCrypt::Password.create(@token),
-          expiry: (Time.now + DeviseTokenAuth.token_lifespan).to_i
-      }
-
+    super do
       @resource.skip_password_validation = true
-      @resource.save
-
-      sign_in(:user, @resource, store: false, bypass: false)
-
-      yield if block_given?
-
-      #update_auth_header
-
-      render_create_success
-
-    else
-      render_create_error_bad_credentials
     end
   end
 
