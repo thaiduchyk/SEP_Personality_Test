72674b85dd65efc15e69faf60463aa1ef3d50753
thaiduchyk <thaiduchyk@gmail.com>
2016-03-28 19:56:01 +0300

    Add changes to SessionsController#destroy

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -23,5 +23,22 @@ class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
     end
   end
 
+  def destroy
+    # remove auth instance variables so that after_action does not run
+    user = remove_instance_variable(:@resource) if @resource
+    client_id = remove_instance_variable(:@client_id) if @client_id
+    remove_instance_variable(:@token) if @token
 
+    if user and client_id and user.tokens[client_id]
+      user.tokens.delete(client_id)
+      user.skip_password_validation = true
+      user.save!
+
+      yield if block_given?
+
+      render_destroy_success
+    else
+      render_destroy_error
+    end
+  end
 end
