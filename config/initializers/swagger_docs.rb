3d778061f5f1f0e8511bb232d0b66517dfe9b50d
Artur Mirda <adrimrutra@gmail.com>
2016-04-21 20:20:58 +0300

    super fix

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -2,6 +2,7 @@ Swagger::Docs::Config.register_apis('1.0' => {
     controller_base_path: 'api/v1',
     api_file_path: 'public/api/v1/',
     clean_directory: true,
-    base_path: Rails.application.config.host,
+  #  base_path: Rails.application.config.host,
+    base_path: 'http://#{uri.host}:#{uri.port}',
     camelize_model_properties: false
 })
