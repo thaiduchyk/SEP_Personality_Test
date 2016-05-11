dacc82d6023eb1cf56bb554f1797744fe2ea126d
yurakravtsiv <yura.kravtsiv@gmail.com>
2016-05-11 16:08:27 +0300

    repair some bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,8 +7,6 @@
 //= require angular-ui-router/release/angular-ui-router
 //= require angular-rails-templates
 //= require angular-ui/build/angular-ui
-//= require angular-bootstrap/ui-bootstrap
-//= require angular-modal-service/modal-service
 
 
 //= require ng-token-auth/dist/ng-token-auth
